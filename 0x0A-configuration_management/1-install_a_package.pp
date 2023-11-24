be1fddd58732f98158f07adfc27ec080331d0121
kipkoechke <chepkwonyke1@gmail.com>
2023-11-24 11:42:41 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,10 +2,10 @@
 
 
 exec {'pip3 install flask':
-  require => Exec['python-installed'],
-  command => '/usr/bin/pip3 install flask==2.1.0'
+require => Exec['python-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
 }
 
 exec {'python-installed':
-  command => '/usr/bin/which python3'
+command => '/usr/bin/which python3'
 }
